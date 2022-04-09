332e7e39f04cebe6344e71e65d6b1e54a3b6427e
Ramitha Manathunga <ramitha.20200910@iit.ac.lk>
2022-04-09 12:54:22 +0530

    finaleApp
    improve the UI and camera activity

--- SDGP-cameraActivity/app/build/tmp/kapt3/stubs/debug/com/example/sVCA8/SignDetectorKt.java
+++
@@ -1,7 +0,0 @@
-package com.example.sVCA8;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 4, 1}, bv = {1, 0, 3}, k = 2, d1 = {"\u0000\u001c\n\u0000\n\u0002\u0018\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000*L\u0010\u0000\"#\u0012\u0019\u0012\u0017\u0012\u0004\u0012\u00020\u00030\u0002\u00a2\u0006\f\b\u0004\u0012\b\b\u0005\u0012\u0004\b\b(\u0006\u0012\u0004\u0012\u00020\u00070\u00012#\u0012\u0019\u0012\u0017\u0012\u0004\u0012\u00020\u00030\u0002\u00a2\u0006\f\b\u0004\u0012\b\b\u0005\u0012\u0004\b\b(\u0006\u0012\u0004\u0012\u00020\u00070\u0001\u00a8\u0006\b"}, d2 = {"signDetectorCallback", "Lkotlin/Function1;", "", "Lcom/example/sVCA8/DetectedSign;", "Lkotlin/ParameterName;", "name", "image", "", "app_debug"})
-public final class SignDetectorKt {
-}
